dd340304ac5aa2b6b2f3dd2a73d9b07ca7a56ad6
Juan Lusardi <juanlusardi@gmail.com>
2017-09-21 01:03:08 -0200

    Creating a Vue Component

--- init.js
+++ init.js
@@ -1,17 +1,23 @@
+Vue.component('person-item', {
+  props: {
+    person: { Type: Object }
+  },
+
+  template: `<li>
+              {{ person.name }}
+             </li>`
+})
+
 const vm = {
   el: '#app',
   data() {
     return {
-      name: 'tHIS IS it'  //[props]
+      persons: [
+      	{ name: 'Ignacio' },
+        { name: 'Cristian '},
+        { name: 'Nicolas '}
+      ]
     }
-  },
-  methods: {
-    //Desde los methods podemos acceder a las [props] usando This.
-    //This es una representacion interna del mismo vm
-    formatName(){
-      this.name = this.name.split(' ').join('-').toLowerCase()
-    }
-
   }
 }
 
